ee246d67a235e5288f709f67b4f3633d49b26d07
shu-illy <sh.illy.9117@gmail.com>
2022-06-23 04:43:36 +0900

    microCMS SDKのクライアント定義

---
+++ src/libs/client.ts
@@ -0,0 +1,7 @@
+import { createClient } from "microcms-js-sdk"; //ES6
+
+// Initialize Client SDK.
+export const client = createClient({
+  serviceDomain: "a05optjcxm", // YOUR_DOMAIN is the XXXX part of XXXX.microcms.io
+  apiKey: "3772786ca5f34eada4850d66ceb64d34ee5f",
+});
