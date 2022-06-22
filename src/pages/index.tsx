daa803d93a98eb04d9cea520db837ab469b2a2f3
shu-illy <sh.illy.9117@gmail.com>
2022-06-23 04:43:44 +0900

    記事の一覧表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,33 @@
-import type { NextPage } from "next";
+import { MicroCMSListResponse } from "microcms-js-sdk";
+import type { GetStaticProps, NextPage } from "next";
+import { client } from "src/libs/client";
 
-const Home: NextPage = () => {
-  return <div className="text-blue-500">Hello!</div>;
+type Blog = {
+  title: string;
+  boty: string;
+};
+
+type Props = MicroCMSListResponse<Blog>;
+
+const Home: NextPage<Props> = (props) => {
+  console.log(props.contents[0].title);
+  return (
+    <div>
+      <p>{`記事の総数: ${props.totalCount}件`}</p>
+      <ul>
+        {props.contents.map((content) => {
+          return <li key={content.id}>{content.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const data = await client.getList({ endpoint: "blog" });
+  return {
+    props: data,
+  };
 };
 
 export default Home;
